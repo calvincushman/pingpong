ea272c974f25927b5f59932374b0158cd8f7292e
Calvin Cushman <calvincushman@gmail.com>
2017-05-05 09:57:55 -0700

    create for loop as well as start if statements for analysing user input

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var userInput = prompt("please input a number");
+
+var countCondition = userInput;
+
+for(i = 0; i <= userInput ; i +=1) {
+
+  if(i % 15 ===0) {
+    i = "Ping-Pong"
+  }
+
+  { break };
+
+  if(i % 3 === 0) {
+    i = "Ping";
+  }
+
+  if(i % 5 === 0) {
+    i = "Pong"
+  }
+
+  if(i <= userInput) {
+
+  }
+}
